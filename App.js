6871c9cdf326ed3aae6fd272f7129ba88a950694
Nicolas F Bergues <nicolasfbergues@MacBook-Pro-de-Nicolas.local>
2018-10-06 14:06:27 -0300

    challenge 7

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+//import { createStackNavigator } from "react-navigation";
+import { TabNavigator, createBottomTabNavigator } from "react-navigation";
+import Screen1 from "./Screen1";
+import Screen2 from "./Screen2";
+import Screen3 from "./Screen3";
+
+/*const RootStack = createStackNavigator({
+  Screen1: {
+    screen: Screen1
+  },
+  Screen2: {
+    screen: Screen2
+  }
+});*/
+const Tabs = createBottomTabNavigator({
+  Screen1: {
+    screen: Screen1
+  },
+  Screen2: {
+    screen: Screen2
+  },
+  Screen3: {
+    screen: Screen3
+  }
+});
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    return <Tabs />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
---
+++ Screen3.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+//import { createStackNavigator } from "react-navigation";
+import { TabNavigator } from "react-navigation";
+
+export default class Screen3 extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Screen 3</Text>
+        {/*
+        
+          <Button
+            title="Ir a Screen 1"
+            onPress={() => this.props.navigation.navigate("Screen1")} jumpTo("Screen1")}
+          />
+        }
+        */}
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
